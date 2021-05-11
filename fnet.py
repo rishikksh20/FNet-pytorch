5bd40bea606f948770aba2b27577a45f4ec80e38
Rishikesh (ऋषिकेश) <rishikksh20@gmail.com>
2021-05-12 00:53:14 +0530

    Create fnet.py

---
+++ fnet.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+from torch import nn
+from einops.layers.torch import Rearrange
+from einops import rearrange, repeat
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+      
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FNetBlock(nn.Module):
+  def __init__(self, dim):
+    super().__init__()
+    self.dim =dim
+
+  def forward(self, x):
+    x = torch.fft.fft(torch.fft.fft(x, dim=-1), dim=-2).real
+    return x
+
+class FNet(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, FNetBlock(dim)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
